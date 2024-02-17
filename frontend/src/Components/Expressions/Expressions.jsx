52e248daa7a7b3faef754a9bb9f27aa3c2d6ff44
Saundq <saundq@gmail.com>
2024-02-17 15:53:49 +0300

    Обновление результатов Правки в readme

--- frontend/src/Components/Expressions/Expressions.jsx
+++ frontend/src/Components/Expressions/Expressions.jsx
@@ -1,5 +1,5 @@
 import style from "./Expressions.module.scss"
-import {Accordion, Container} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import {useEffect, useState} from "react";
 
 
@@ -22,6 +22,8 @@ export const Expressions = () => {
         }
 
         fetchData();
+        const interval = setInterval(fetchData, 5000);
+        return () => clearInterval(interval);
     }, []);
 
     return (
@@ -59,7 +61,7 @@ export const Expressions = () => {
                                     return (
                                         <tr key={k}>
                                             <td><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" className="bi bi-arrow-return-right" viewBox="0 0 16 16">
-                                                <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
+                                                <path fillRule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5"/>
                                             </svg></td>
                                             <td>{v.id}</td>
                                             <td>{v.expression_string } {v.next!==null ? " Вычисляется после "+v.next:""} {v.previous!=null ? "Вычисляется после "+v.previous:""}</td>
